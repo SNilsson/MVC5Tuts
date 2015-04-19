4a7ee88b5ca5b053ba6779a5d8c3105ae8dad881
SNilsson <simon.simon.nilsson@gmail.com>
2015-04-19 10:24:48 +0200

    First commit with the routing example

---
+++ FirstController/App_Start/RouteConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FirstController
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //product/cpu/11
+
+            routes.MapMvcAttributeRoutes();
+
+           // routes.MapRoute(
+           //    name: "Products",
+           //    url: "products/{products}/{id}",
+           //    defaults: new { 
+           //        controller = "products", 
+           //        action = "Index", 
+           //        catagory = UrlParameter.Optional,
+           //        id = 0
+           //    },
+           //    constraints: new {id = @"\d+"}
+           //);
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ FirstController/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstController.Controllers
+{
+    [RoutePrefix("products")]
+    public class ProductsController : Controller
+    {
+        // GET: Product
+
+        public string Index()
+        {
+            return "this is all the products";
+        }
+
+        [Route("{catagory}")]
+        public string showCatagory(string products){
+            return "This is the list of products within the catagory";
+        }
+         [Route("{catagory?}/{id:int}")]
+        public string details(string catagory, string id){
+            return "This is the details about the certain product with ID: " + id;
+        }
+        [Route("~/for-sale/{catagory?}")]
+        public string foo(string id)
+        {
+            return "this is a method action " + id;
+        }
+    }
+}
